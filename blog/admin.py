374e8e9adb8783d0c84f4e834bced607c4b2008f
Rômulo Furtado <romuloros@gmail.com>
2021-03-17 14:26:31 +0100

    feat: improve post admin model

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on', 'author')
+    list_filter = ("status",)
+    search_fields = [
+        'title', 'content', 'author__first_name', 'author__last_name', 'author__username'
+    ]
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
