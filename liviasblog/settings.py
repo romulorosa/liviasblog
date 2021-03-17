c0cd5fd21bef5bb16753838858b77fe741ca5cde
Rômulo Furtado <romuloros@gmail.com>
2021-03-17 16:20:34 +0100

    feat: introduce django-quill-editor

--- liviasblog/settings.py
+++ liviasblog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'django_quill',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
